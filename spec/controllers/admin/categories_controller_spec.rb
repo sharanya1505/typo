fa8879ba6e610f5e100e4cc127c2b5cf3b49a630
sharanya1505 <sharanyashivakumar150593@gmail.com>
2016-05-07 00:00:26 +0000

    New Changes

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -1,71 +1,3 @@
-=begin
-require 'spec_helper'
-
-describe Admin::CategoriesController do
-  render_views
-
-  before(:each) do
-    Factory(:blog)
-    #TODO Delete after removing fixtures
-    Profile.delete_all
-    henri = Factory(:user, :login => 'henri', :profile => Factory(:profile_admin, :label => Profile::ADMIN))
-    request.session = { :user => henri.id }
-  end
-
-  it "test_index" do
-    get :index
-    assert_response :redirect, :action => 'index'
-  end
-
-  describe "test_edit" do
-    before(:each) do
-      get :edit, :id => Factory(:category).id
-    end
-
-    it 'should render template new' do
-      assert_template 'new'
-      assert_tag :tag => "table",
-        :attributes => { :id => "category_container" }
-    end
-
-    it 'should have valid category' do
-      assigns(:category).should_not be_nil
-      assert assigns(:category).valid?
-      assigns(:categories).should_not be_nil
-    end
-  end
-
-  it "test_update" do
-    post :edit, :id => Factory(:category).id
-    assert_response :redirect, :action => 'index'
-  end
-
-  describe "test_destroy with GET" do
-    before(:each) do
-      test_id = Factory(:category).id
-      assert_not_nil Category.find(test_id)
-      get :destroy, :id => test_id
-    end
-
-    it 'should render destroy template' do
-      assert_response :success
-      assert_template 'destroy'      
-    end
-  end
-
-  it "test_destroy with POST" do
-    test_id = Factory(:category).id
-    assert_not_nil Category.find(test_id)
-    get :destroy, :id => test_id
-
-    post :destroy, :id => test_id
-    assert_response :redirect, :action => 'index'
-
-    assert_raise(ActiveRecord::RecordNotFound) { Category.find(test_id) }
-  end
-  
-end
-=end
 require 'spec_helper'
 
 describe Admin::CategoriesController do
@@ -85,24 +17,16 @@ describe Admin::CategoriesController do
   end
 
   describe "test_new" do
-    before(:each) do
-      get :edit, :id => Factory(:category).id
-    end
-
-    it 'should render template new' do
-      assert_template 'new'
-      assert_tag :tag => "table",
-        :attributes => { :id => "category_container" }
-    end
-
-    it 'should have valid category' do
-      assigns(:category).should_not be_nil
-      assert assigns(:category).valid?
-      assigns(:categories).should_not be_nil
-    end
-  end
-
-
+     before(:each) do
+     get :new
+   end
+   
+   it "creates new category" do
+#     assign(:category, Factory(:category))
+     assigns[:category] = Factory(:category)
+     post :new, :name => Factory(:category).name, :permalink => Factory(:category).permalink
+   end
+ end
 
 
   describe "test_edit" do
@@ -153,4 +77,3 @@ describe Admin::CategoriesController do
   end
   
 end
-
